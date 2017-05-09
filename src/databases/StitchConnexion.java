ac1f695c28d56f03c8322dda26fd411a074ddb71
BBchat <celeste.allardi@telecomnancy.net>
2017-05-09 14:43:38 +0200

    Parser Stitch

---
+++ src/databases/StitchConnexion.java
@@ -0,0 +1,41 @@
+package databases;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class StitchConnexion {
+	
+	public static void main(String[] args) {
+		
+		stitch();
+	}
+	
+	public static void stitch() {
+
+	        String csvFile = "resourcesFiles/chemical.sources.v5.0.EXTRAIT.tsv";
+	        String line = "";
+	        String cvsSplitBy = "\t";
+
+	        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+	        	
+	        	line = br.readLine();
+	            while ((line = br.readLine()) != null) {
+	            	if (!line.startsWith("#")) {
+	                	String[] uneLigne = line.split(cvsSplitBy);
+	                	System.out.println(uneLigne[0] + " " + uneLigne[1] + " " + uneLigne[2] + " " + uneLigne[3]);
+	            	}
+
+	            }
+
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+	    }
+	
+	
+	
+
+}
