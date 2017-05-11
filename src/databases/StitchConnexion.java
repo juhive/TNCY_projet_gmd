de731066c70364aea3e89fd3280a5333fb459c6d
justine <justine>
2017-05-11 16:26:40 +0200

    lien id atc stitch

--- src/databases/StitchConnexion.java
+++ src/databases/StitchConnexion.java
@@ -8,14 +8,16 @@ import java.io.IOException;
 public class StitchConnexion {
 	
 	public static void main(String[] args) {
-		stitch();
+		String ATC = stitch("CIDm00010517");
+		System.out.println(ATC);
 	}
 	
-	public static void stitch() {
+	public static String stitch(String coumpound_id) {
 
 	        String csvFile = "resourcesFiles/Stitch/chemical.sources.v5.0.EXTRAIT.tsv";
 	        String line = "";
 	        String cvsSplitBy = "\t";
+	        String codeATC = "no match";
 
 	        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
 	        	
@@ -23,7 +25,12 @@ public class StitchConnexion {
 	            while ((line = br.readLine()) != null) {
 	            	if (!line.startsWith("#")) {
 	                	String[] uneLigne = line.split(cvsSplitBy);
-	                	System.out.println(uneLigne[0] + " " + uneLigne[1] + " " + uneLigne[2] + " " + uneLigne[3]);
+	                	//System.out.println(uneLigne[0] + " " + uneLigne[1] + " " + uneLigne[2] + " " +uneLigne[3]);
+	                	System.out.println(uneLigne[0].toString()+"   "+coumpound_id);
+	                	
+	                	if (uneLigne[0].toString().equals(coumpound_id)){
+	                		codeATC =uneLigne[3];
+	                	}
 	            	}
 
 	            }
@@ -31,6 +38,7 @@ public class StitchConnexion {
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }
+	        return codeATC;
 
 	    }
 	
